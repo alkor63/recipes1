587194bace9ad0baee59eec6dc54cf9680e850fa
alkor63 <alkor@medi-spa.ru>
2023-03-05 01:45:59 +0300

    first commit

---
+++ src/test/java/pro/sky/recipesbook/RecipesBookApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.recipesbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
